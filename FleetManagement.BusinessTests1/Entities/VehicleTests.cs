a53e07b49898da013e6a39e7575f5f2fe73697c9
wauterstoon <toon.Wauters@student.hogent.be>
2021-10-13 14:27:30 +0200

    UnitTests Driver

--- FleetManagement.BusinessTests1/Entities/VehicleTests.cs
+++ FleetManagement.BusinessTests1/Entities/VehicleTests.cs
@@ -11,16 +11,26 @@ namespace FleetManagement.Business.Entities.Tests
     [TestClass()]
     public class VehicleTests
     {
+        
         [TestMethod()]
         public void VehicleTest()
         {
-            throw new NotImplementedException();
+            
+          
+
         }
 
         [TestMethod()]
         public void SetVehicleIdTest()
         {
-            throw new NotImplementedException();
+            var vecId = 1;
+            var time = DateTime.DaysInMonth(2000, 10);
+
+
+            Vehicle v = new Vehicle(vecId,"","","","","","","",5, new Driver("","",new DateTime(20,10,01), "00.06.30-283.52",new List<string>()));
+
+            Assert.AreEqual(1, vecId);
+
         }
 
         [TestMethod()]
