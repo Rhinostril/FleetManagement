b0541eeae11705a88a8bf77a6bc398177a0b2e90
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-15 12:04:58 +0200

    updated IDriverRepo

--- FleetManagement/Interfaces/IDriverRepository.cs
+++ FleetManagement/Interfaces/IDriverRepository.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FleetManagement.Business.Entities;
 
 namespace FleetManagement.Business.Interfaces
 {
     public interface IDriverRepository
     {
 
-
+        IReadOnlyList<Driver> GetAllDrivers();
+        IReadOnlyList<Driver> SearchDrivers(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address);
+        Driver GetDriver(); //overbodig?
+        Driver SearchDriver(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address);
+        bool DriverExists(int id);
+        void AddDriver(Driver driver);
+        void UpdateDriver(Driver driver);
+        void DeleteDriver(Driver driver);
 
     }
 }
