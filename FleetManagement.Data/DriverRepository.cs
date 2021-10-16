2aa15aae6b8206b5d2325af4ac21b14f8580368e
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-16 14:45:11 +0200

    DriverRepository in DataLayer

---
+++ FleetManagement.Data/DriverRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using FleetManagement.Business.Entities;
+using FleetManagement.Business.Interfaces;
+
+namespace FleetManagement.Data
+{
+    public class DriverRepository : IDriverRepository
+    {
+
+
+
+        public void AddDriver(Driver driver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDriver(Driver driver)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DriverExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Driver> GetAllDrivers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Driver GetDriver()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Driver SearchDriver(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<Driver> SearchDrivers(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDriver(Driver driver)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
