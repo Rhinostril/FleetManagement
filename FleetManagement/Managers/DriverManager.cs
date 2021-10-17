2b3eeb8bf88aa683297396633db90a5d6792e5da
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-17 13:47:26 +0200

    hoe werkt repo en manager

--- FleetManagement/Managers/DriverManager.cs
+++ FleetManagement/Managers/DriverManager.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FleetManagement.Business.Entities;
+using FleetManagement.Business.Interfaces;
 
 namespace FleetManagement.Business.Managers
 {
     public class DriverManager
     {
 
-        
+        private IDriverRepository repo; //feedback leerkracht
 
     }
 }
