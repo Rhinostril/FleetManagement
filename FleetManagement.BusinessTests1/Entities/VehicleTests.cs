086efe4fed0d9406b749a22c543ce6dbd92108ee
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-19 13:51:21 +0200

    revision vehicle tests

--- FleetManagement.BusinessTests1/Entities/VehicleTests.cs
+++ FleetManagement.BusinessTests1/Entities/VehicleTests.cs
@@ -29,8 +29,7 @@ namespace FleetManagement.Business.Entities.Tests
         [Fact]
         public void Test_SetVehicleId_Valid()
         {
-            Driver driver = new Driver("Tino", "Pijpers", new DateTime(), "97.05.20-327.78", new List<string> { "B" });
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, driver);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetVehicleId(1);
             Assert.Equal(1, vehicle.VehicleId);
         }
@@ -40,14 +39,14 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(0)]
         public void Test_SetVehicleId_Invalid(int id)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetVehicleId(id));
         }
 
         [Fact]
         public void Test_SetBrand_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetBrand("BMW");
             Assert.Equal("BMW", vehicle.Brand);
         }
@@ -57,14 +56,14 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(null)]
         public void Test_SetBrand_Invalid(string brand)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetBrand(brand));
         }
 
         [Fact]
         public void Test_SetModel_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetModel("M5");
             Assert.Equal("M5", vehicle.Model);
         }
@@ -74,14 +73,14 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(null)]
         public void Test_SetModel_Invalid(string model)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetModel(model));
         }
 
         [Fact]
         public void Test_SetChassisNumber_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetChassisNumber("4444-4444-4444-17");
             Assert.Equal("4444-4444-4444-17", vehicle.ChassisNumber);
         }
@@ -92,14 +91,14 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(null)]
         public void Test_SetChassisNumber_Invalid(string chassisNumber)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetChassisNumber(chassisNumber));
         }
 
         [Fact]
         public void Test_SetLicensePlate_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetLicensePlate("KAPPER IMRE");
             Assert.Equal("KAPPER IMRE", vehicle.LicensePlate);
         }
@@ -107,17 +106,16 @@ namespace FleetManagement.Business.Entities.Tests
         [Theory]
         [InlineData(null)]
         [InlineData("")]
-        //[InlineData("ehtfkljagryvrayevgtgburykotuklhilmh9olohl;0")]  license plate max characters ?
         public void Test_SetLicensePlate_Invalid(string plate)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetLicensePlate(plate));
         }
 
         [Fact]
         public void Test_SetFuelType_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetFuelType(new FuelType("Diesel"));
             Assert.Equal("Diesel", vehicle.FuelType.FuelName);
         }
@@ -126,14 +124,14 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(null)]
         public void Test_SetFuelType_Invalid(FuelType fuelType)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetFuelType(fuelType));
         }
 
         [Fact]
         public void Test_SetVehicleType_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetVehicleType("personenwagen");
             Assert.Equal("personenwagen", vehicle.VehicleType);
         }
@@ -143,14 +141,14 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(null)]
         public void Test_SetVehicleType_Invalid(string type)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetVehicleType(type));
         }
 
         [Fact]
         public void Test_SetVehicleColor_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetVehicleColor("Red");
             Assert.Equal("Red", vehicle.Color);
         }
@@ -160,14 +158,14 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(null)]
         public void Test_SetVehicleColor_Invalid(string color)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetVehicleColor(color));
         }
 
         [Fact]
         public void Test_SetVehicleDoors_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             vehicle.SetVehicleDoors(2);
             Assert.Equal(2, vehicle.Doors);
         }
@@ -180,20 +178,44 @@ namespace FleetManagement.Business.Entities.Tests
         [InlineData(27)]
         public void Test_SetVehicleDoors_Invalid(int doors)
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Assert.Throws<VehicleException>(() => vehicle.SetVehicleDoors(doors));
         }
 
         [Fact]
         public void Test_SetDriver_Valid()
         {
-            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2, null);
+            Vehicle vehicle = new Vehicle(1, "Porsche", "GT2RS", "1234-1234-1234-17", "KAPPER FURKAN", new FuelType("Gasoline"), "Sportauto", "Donkergrijs", 2);
             Driver driver = new Driver("Elvis", "Presley", new DateTime(1997, 05, 20), "97.05.20-327.78", new List<string> { "B", "A1" });
             vehicle.SetDriver(driver);
             Assert.Equal(driver, vehicle.Driver);
         }
 
         //TODO Test_SetDriver_Invalid()
+
+        [Fact]
+        public void Test_HasDriver_Valid()
+        {
+
+        }
+
+        [Fact]
+        public void Test_HasDriver_Invalid()
+        {
+
+        }
+
+        [Fact]
+        public void Test_RemoveDriver_Valid()
+        {
+
+        }
+
+        [Fact]
+        public void Test_RemoveDriver_Invalid()
+        {
+
+        }
 
     }
 }
