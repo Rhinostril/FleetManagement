fd5d012b1ca98398a21f830bd922817a083674ac
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-19 15:18:34 +0200

    Updated vehicleManager + exception

---
+++ FleetManagement/ManagerExceptions/VehicleManagerException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FleetManagement.Business.Managers
+{
+    public class VehicleManagerException : Exception
+    {
+        public VehicleManagerException()
+        {
+        }
+
+        public VehicleManagerException(string message) : base(message)
+        {
+        }
+
+        public VehicleManagerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
--- FleetManagement/Managers/VehicleManager.cs
+++ FleetManagement/Managers/VehicleManager.cs
@@ -3,13 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FleetManagement.Business.Entities;
+using FleetManagement.Business.Interfaces;
 
 namespace FleetManagement.Business.Managers
 {
     public class VehicleManager
     {
 
+        private IVehicleRepository repo;
 
+        public VehicleManager(IVehicleRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            try
+            {
+                if (!repo.VehicleExists(vehicle))
+                {
+                    repo.AddVehicle(vehicle);
+                }
+                else
+                {
+                    throw new VehicleManagerException("VehicleManager - AddVehicle - Vehicle already added");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void UpdateVehicle(Vehicle vehicle)
+        {
+            try
+            {
+                // Bestaat vehicle met properties al ?
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void DeleteVehicle(Vehicle vehicle)
+        {
+            try
+            {
+                if (repo.VehicleExists(vehicle))
+                {
+                    repo.DeleteVehicle(vehicle);
+                }
+                else
+                {
+                    throw new VehicleManagerException("VehicleManager - DeleteVehicle - Vehicle already deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
     }
 }
