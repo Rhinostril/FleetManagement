247cf0d440f0a483ef165ec29ce955bfc68c6cc1
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-19 14:57:20 +0200

    Updated FuelCardManager + exception

---
+++ FleetManagement/ManagerExceptions/FuelCardManagerException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FleetManagement.Business.Managers
+{
+    public class FuelCardManagerException : Exception
+    {
+        public FuelCardManagerException()
+        {
+        }
+
+        public FuelCardManagerException(string message) : base(message)
+        {
+        }
+
+        public FuelCardManagerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
--- FleetManagement/Managers/FuelCardManager.cs
+++ FleetManagement/Managers/FuelCardManager.cs
@@ -3,13 +3,77 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FleetManagement.Business.Interfaces;
+using FleetManagement.Business.Entities;
 
 namespace FleetManagement.Business.Managers
 {
     public class FuelCardManager
     {
 
+        private IFuelCardRepository repo;
 
+        public FuelCardManager(IFuelCardRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public void AddFuelCard(FuelCard fuelCard)
+        {
+            try
+            {
+                if (!repo.FuelCardExists(fuelCard))
+                {
+                    repo.AddFuelCard(fuelCard);
+                }
+                else
+                {
+                    throw new FuelCardManagerException("FuelCardManager - AddFuelCard - FuelCard already added");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void UpdateFuelCard(FuelCard fuelCard)
+        {
+            try
+            {
+                if (!repo.FuelCardExists(fuelCard))
+                {
+                    repo.AddFuelCard(fuelCard);
+                }
+                else
+                {
+                    throw new FuelCardManagerException("FuelCardManager - UpdateFuelCard - FuelCard already exists");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void DeleteFuelCard(FuelCard fuelCard)
+        {
+            try
+            {
+                if (repo.FuelCardExists(fuelCard))
+                {
+                    repo.DeleteFuelCard(fuelCard);
+                }
+                else
+                {
+                    throw new FuelCardManagerException("FuelCardManager - DeleteFuelCard - FuelCard already deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
     }
 }
