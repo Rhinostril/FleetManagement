bb0b9ee8de1859c3a10dc9914dc238373554eca5
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-19 15:22:16 +0200

    Added repositories in data layer

---
+++ FleetManagement.Data/Repositories/VehicleRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using FleetManagement.Business.Entities;
+using FleetManagement.Business.Interfaces;
+
+namespace FleetManagement.Data
+{
+    public class FuelCardRepository : IFuelCardRepository
+    {
+        public void AddFuelCard(FuelCard fuelCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFuelCard(FuelCard fuelCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FuelCardExists(FuelCard fuelCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<FuelCard> GetAllFuelCards()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FuelCard GetFuelCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FuelCard SearchFuelCard(int? fuelCardId, string cardNr, FuelType fuelType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<FuelCard> SearchFuelCards(int? fuelCardId, string cardNr, FuelType fuelType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFuelCard(FuelCard fuelCard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
