53139a05a8c708fdbb65f247f411c84c51607bf0
TinoPijpers <tino.pijpers@student.hogent.be>
2021-10-25 16:37:27 +0200

    qqq

--- FleetManagement.Data/Repositories/FuelCardRepository.cs
+++ FleetManagement.Data/Repositories/FuelCardRepository.cs
@@ -6,55 +6,13 @@ using System.Data.SqlClient;
 
 namespace FleetManagement.Data
 {
-    public class DriverRepository : IDriverRepository
+    public class FuelCardRepository : IFuelCardRepository
     {
 
         private string connectionString = $"Data Source=fleetmanagserver.database.windows.net;Persist Security Info=True;User ID=fleetadmin;Password=***********";
 
-        private SqlConnection getConnection()
-        {
-            SqlConnection connection = new SqlConnection(connectionString);
-            return connection;
-        }
+        
 
-        public void AddDriver(Driver driver)
-        {
-            throw new NotImplementedException();
-        }
 
-        public void DeleteDriver(Driver driver)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool DriverExists(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IReadOnlyList<Driver> GetAllDrivers()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Driver GetDriver()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Driver SearchDriver(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IReadOnlyList<Driver> SearchDrivers(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateDriver(Driver driver)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
