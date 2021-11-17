2f02c96493bacf37440eceb52369721c36765aca
Rhinostril <stan.matthijs@student.hogent.be>
2021-11-17 11:02:45 +0100

    Take 50 and take any amount methods in repo

--- FleetManagement/Interfaces/IDriverRepository.cs
+++ FleetManagement/Interfaces/IDriverRepository.cs
@@ -4,10 +4,11 @@ using FleetManagement.Business.Entities;
 
 namespace FleetManagement.Business.Interfaces
 {
-    public interface IDriverRepository
-    {
+    public interface IDriverRepository {
 
         IReadOnlyList<Driver> GetAllDrivers();
+        IReadOnlyList<Driver> GetTop50Drivers();
+        IReadOnlyList<Driver> GetDriversByAmount(int amount);
         IReadOnlyList<Driver> SearchDrivers(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address);
         Driver GetDriverById(int id);
         Driver SearchDriver(int? id, string firstName, string lastName, DateTime dateOfBirth, Address address);
--- FleetManagement/Interfaces/IVehicleRepository.cs
+++ FleetManagement/Interfaces/IVehicleRepository.cs
@@ -10,6 +10,8 @@ namespace FleetManagement.Business.Interfaces
     public interface IVehicleRepository
     {
         IReadOnlyList<Vehicle> GetAllVehicles();
+        IReadOnlyList<Vehicle> GetTop50Vehicles();
+        IReadOnlyList<Vehicle> GetVehiclesByAmount(int amount);
         IReadOnlyList<Vehicle> SearchVehicles(int? vehicleId, string brand, string model, string chassisNumber, string licensePlate, FuelType fuelType, string vehicleType, string color, int doors, Driver driver);
         Vehicle GetVehicle(int vehicleId);
         Vehicle SearchVehicle(int? vehicleId, string brand, string model, string chassisNumber, string licensePlate, FuelType fuelType, string vehicleType, string color, int doors, Driver driver);
