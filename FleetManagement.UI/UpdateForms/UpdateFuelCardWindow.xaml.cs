bb79e572e65b3792c19ecec15d522a09e63621ac
TinoPijpers <tino.pijpers@student.hogent.be>
2021-12-07 12:28:42 +0100

    added update fuelCard Window

---
+++ FleetManagement.UI/UpdateForms/UpdateFuelCardWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FleetManagement.UI.UpdateForms
+{
+    /// <summary>
+    /// Interaction logic for UpdateFuelCardWindow.xaml
+    /// </summary>
+    public partial class UpdateFuelCardWindow : Window
+    {
+        public UpdateFuelCardWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
